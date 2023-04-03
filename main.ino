87d4580b400b25c243fff652b09a0bc0d7d3196b
Moisés Corrêa <54242235+skittlexyz@users.noreply.github.com>
2023-04-03 15:28:41 -0300

    Add files via upload

--- main.ino
+++ main.ino
@@ -5,15 +5,16 @@
 
 /* Pin declaration section */
 // IR Sensor pins
-byte irSensorPins [] = {};
+uint8_t irSensorPins[] = {};
 // Grouped Stepper Motor pins {step pin, direction pin}, enable pin
-byte stepperPins [] = {{2,5},{3,6},8};
+uint8_t stepperPins[5] = {2,5,3,6,8};
 
 /* Function declaration section */
 // IR Sensor Reader receives the sensor pin
 int irSensorRead(byte pin);
-// Stepper Controller receiver if the movement will be in a cross axis, the step quantity, the direction (F, B, L, R) if the cross is true and the angle if it isn't.
-bool stepperControl(bool cross, int steps, char direction, int angle);
+
+// Stepper Controller receiver if the movement will be in a cross axis, the delay between the steps, the step quantity, the direction (F, B, L, R) if the cross is true and the angle if it isn't.
+bool stepperControl(bool cross, int velocity, int steps, int direction, int angle);
 // Servo Controller receives which servo to control and the movement method
 bool servoControl(byte current, char method);
 // Pins configuration setup
@@ -24,60 +25,60 @@ void setup() {
   setupPins();
 }
 void loop() {
-  stepperControl(true, 500, "F", 0);
-  stepperControl(true, 500, "B", 0);
-  stepperControl(true, 500, "L", 0);
-  stepperControl(true, 500, "R", 0);
+  stepperControl(true, 1, 500, 0, 0);
+  stepperControl(true, 1, 500, 1, 0);
+  stepperControl(true, 1, 500, 2, 0);
+  stepperControl(true, 1, 500, 3, 0);
 }
 
 /* Functions section */
 int irSensorRead(byte pin) {
   return 0;
 }
-bool stepperControl(bool cross, int steps, char direction, int angle) {
+bool stepperControl(bool cross, int velocity, int steps, int direction, int angle) {
   bool directionToggleAssistant = HIGH;
   if (cross) {
     for (int i = 0; i < steps; i++) {
       switch (direction) {
-      case "F":
-        digitalWrite(stepperPins[0][1], directionToggleAssistant);
-        digitalWrite(stepperPins[1][1], directionToggleAssistant);
-        digitalWrite(stepperPins[0][0], HIGH);
-        digitalWrite(stepperPins[1][0], HIGH);
-        delay(1);
-        digitalWrite(stepperPins[0][0], LOW);
-        digitalWrite(stepperPins[1][0], LOW);
-        delay(1);      
+      case 0:
+        digitalWrite(stepperPins[1], directionToggleAssistant);
+        digitalWrite(stepperPins[3], directionToggleAssistant);
+        digitalWrite(stepperPins[0], HIGH);
+        digitalWrite(stepperPins[2], HIGH);
+        delay(velocity);
+        digitalWrite(stepperPins[0], LOW);
+        digitalWrite(stepperPins[2], LOW);
+        delay(velocity);      
         break;
-      case "B":
-        digitalWrite(stepperPins[0][1], !directionToggleAssistant);
-        digitalWrite(stepperPins[1][1], !directionToggleAssistant);
-        digitalWrite(stepperPins[0][0], HIGH);
-        digitalWrite(stepperPins[1][0], HIGH);
-        delay(1);
-        digitalWrite(stepperPins[0][0], LOW);
-        digitalWrite(stepperPins[1][0], LOW);
-        delay(1);      
+      case 1:
+        digitalWrite(stepperPins[1], !directionToggleAssistant);
+        digitalWrite(stepperPins[3], !directionToggleAssistant);
+        digitalWrite(stepperPins[0], HIGH);
+        digitalWrite(stepperPins[2], HIGH);
+        delay(velocity);
+        digitalWrite(stepperPins[0], LOW);
+        digitalWrite(stepperPins[2], LOW);
+        delay(velocity);      
         break;
-      case "L":
-        digitalWrite(stepperPins[0][1], directionToggleAssistant);
-        digitalWrite(stepperPins[1][1], !directionToggleAssistant);
-        digitalWrite(stepperPins[0][0], HIGH);
-        digitalWrite(stepperPins[1][0], HIGH);
-        delay(1);
-        digitalWrite(stepperPins[0][0], LOW);
-        digitalWrite(stepperPins[1][0], LOW);
-        delay(1);      
+      case 2:
+        digitalWrite(stepperPins[1], directionToggleAssistant);
+        digitalWrite(stepperPins[3], !directionToggleAssistant);
+        digitalWrite(stepperPins[0], HIGH);
+        digitalWrite(stepperPins[2], HIGH);
+        delay(velocity);
+        digitalWrite(stepperPins[0], LOW);
+        digitalWrite(stepperPins[2], LOW);
+        delay(velocity);      
         break;
-      case "R":
-        digitalWrite(stepperPins[0][1], !directionToggleAssistant);
-        digitalWrite(stepperPins[1][1], directionToggleAssistant);
-        digitalWrite(stepperPins[0][0], HIGH);
-        digitalWrite(stepperPins[1][0], HIGH);
-        delay(1);
-        digitalWrite(stepperPins[0][0], LOW);
-        digitalWrite(stepperPins[1][0], LOW);
-        delay(1);      
+      case 3:
+        digitalWrite(stepperPins[1], !directionToggleAssistant);
+        digitalWrite(stepperPins[3], directionToggleAssistant);
+        digitalWrite(stepperPins[0], HIGH);
+        digitalWrite(stepperPins[2], HIGH);
+        delay(velocity);
+        digitalWrite(stepperPins[0], LOW);
+        digitalWrite(stepperPins[2], LOW);
+        delay(velocity);      
         break;
       }
     }
@@ -91,13 +92,11 @@ bool servoControl(byte current, char method) {
   return true;
 }
 bool setupPins() {
-  for (int i = 0; i < getSize(stepperPins) - 1; i++) {
-    for (int j = 0; j < getSize(stepperPins[i]); j++) {
-      pinMode(stepperPins[i][j], OUTPUT);
-    }
+  for (int i = 0; i < sizeof(stepperPins) - 1; i++) {
+    pinMode(stepperPins[i], OUTPUT);
   }
-  pinMode(stepperPins[getSize(stepperPins) - 1], OUTPUT);
-  for (int i = 0; i < getSize(irSensorPins); i++) {
+  pinMode(stepperPins[sizeof(stepperPins) - 1], OUTPUT);
+  for (int i = 0; i < sizeof(irSensorPins); i++) {
     pinMode(irSensorPins[i], INPUT);
   }
   return true;

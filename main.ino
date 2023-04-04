b3cd893c01615042a0d78757c166a700f4b543bc
Moisés Corrêa <54242235+skittlexyz@users.noreply.github.com>
2023-04-04 08:37:46 -0300

    Add files via upload

--- main.ino
+++ main.ino
@@ -2,6 +2,7 @@
 
 /* General configurations section */
 // WIP
+const int velocity = 300;
 
 /* Pin declaration section */
 // IR Sensor pins
@@ -11,38 +12,43 @@ uint8_t stepperPins[5] = {2,5,3,6,8};
 
 /* Function declaration section */
 // IR Sensor Reader receives the sensor pin
-int irSensorRead(byte pin);
+int irSensorRead(int pin);
 
 // Stepper Controller receiver if the movement will be in a cross axis, the delay between the steps, the step quantity, the direction (F, B, L, R) if the cross is true and the angle if it isn't.
 bool stepperControl(bool cross, int velocity, int steps, int direction, int angle);
 // Servo Controller receives which servo to control and the movement method
-bool servoControl(byte current, char method);
+bool servoControl(int current, char method);
+// Prints all the important info
+void infoPrint();
+// Try the function
+bool tryFunction(bool current, String name, String error);
 // Pins configuration setup
 bool setupPins();
 
 /* Main code section */
 void setup() {
-  setupPins();
+  Serial.begin(115200);
+  infoPrint();
+  tryFunction(setupPins(), "setupPins()", "error");
 }
 void loop() {
-  stepperControl(true, 1, 500, 0, 0);
-  stepperControl(true, 1, 500, 1, 0);
-  stepperControl(true, 1, 500, 2, 0);
-  stepperControl(true, 1, 500, 3, 0);
+  stepperControl(true, velocity, 500, 0, 0);
+  stepperControl(true, velocity, 500, 1, 0);
+  stepperControl(true, velocity, 500, 2, 0);
+  stepperControl(true, velocity, 500, 3, 0);
 }
 
 /* Functions section */
-int irSensorRead(byte pin) {
+int irSensorRead(int pin) {
   return 0;
 }
 bool stepperControl(bool cross, int velocity, int steps, int direction, int angle) {
-  bool directionToggleAssistant = HIGH;
   if (cross) {
     for (int i = 0; i < steps; i++) {
       switch (direction) {
       case 0:
-        digitalWrite(stepperPins[1], directionToggleAssistant);
-        digitalWrite(stepperPins[3], directionToggleAssistant);
+        digitalWrite(stepperPins[1], HIGH);
+        digitalWrite(stepperPins[3], LOW);
         digitalWrite(stepperPins[0], HIGH);
         digitalWrite(stepperPins[2], HIGH);
         delay(velocity);
@@ -51,8 +57,8 @@ bool stepperControl(bool cross, int velocity, int steps, int direction, int angl
         delay(velocity);      
         break;
       case 1:
-        digitalWrite(stepperPins[1], !directionToggleAssistant);
-        digitalWrite(stepperPins[3], !directionToggleAssistant);
+        digitalWrite(stepperPins[1], LOW);
+        digitalWrite(stepperPins[3], HIGH);
         digitalWrite(stepperPins[0], HIGH);
         digitalWrite(stepperPins[2], HIGH);
         delay(velocity);
@@ -61,8 +67,8 @@ bool stepperControl(bool cross, int velocity, int steps, int direction, int angl
         delay(velocity);      
         break;
       case 2:
-        digitalWrite(stepperPins[1], directionToggleAssistant);
-        digitalWrite(stepperPins[3], !directionToggleAssistant);
+        digitalWrite(stepperPins[1], HIGH);
+        digitalWrite(stepperPins[3], HIGH);
         digitalWrite(stepperPins[0], HIGH);
         digitalWrite(stepperPins[2], HIGH);
         delay(velocity);
@@ -71,8 +77,8 @@ bool stepperControl(bool cross, int velocity, int steps, int direction, int angl
         delay(velocity);      
         break;
       case 3:
-        digitalWrite(stepperPins[1], !directionToggleAssistant);
-        digitalWrite(stepperPins[3], directionToggleAssistant);
+        digitalWrite(stepperPins[1], LOW);
+        digitalWrite(stepperPins[3], LOW);
         digitalWrite(stepperPins[0], HIGH);
         digitalWrite(stepperPins[2], HIGH);
         delay(velocity);
@@ -88,7 +94,14 @@ bool stepperControl(bool cross, int velocity, int steps, int direction, int angl
   }
   return false;
 }
-bool servoControl(byte current, char method) {
+bool servoControl(int current, char method) {
+  return true;
+}
+bool tryFunction(bool current, String name, String error) {
+  while (!current) {
+    Serial.println(name + " " + error + "!");
+    while (true) {}
+  }
   return true;
 }
 bool setupPins() {

388509ec615f98069d38935479bf954c978fbb07
Moisés Corrêa <54242235+skittlexyz@users.noreply.github.com>
2023-06-05 17:26:53 -0300

    Add files via upload

--- OBR-2023-main.ino
+++ OBR-2023-main.ino
@@ -4,14 +4,17 @@
 #include <U8g2lib.h>
 #include "Adafruit_TCS34725softi2c.h"
 #include <Wire.h>
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
 
 /* General configurations section */
 // Delay between each stepper step
 #define stepperVelocity 1
 #define oledDisplay true
 #define colorVelocity TCS34725_INTEGRATIONTIME_50MS
+#define INTERRUPT_PIN 2 
 
-/* Pin declaration section */
+/* Pin declaration and variables section */
 // IR Sensor pins
 uint8_t irSensorPins[8] = {34,35,36,37,38,39,40,41};
 // Ultrassonic Pins {trig, echo, ...}
@@ -20,6 +23,16 @@ uint8_t ultraPins[8] = {42,43,44,45,46,47,48,49};
 uint8_t stepperPins[5] = {2,5,3,6,8};
 // TCS34725 Pins {sda, scl, ...}
 uint8_t colorPins[4] = {50,51,52,53};
+// MPU6050 Ambient Variables
+bool dmpReady = false;
+uint8_t mpuIntStatus;
+uint8_t devStatus;
+uint16_t packetSize;
+uint16_t fifoCount;
+uint8_t fifoBuffer[64];
+Quaternion q;
+uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
+volatile bool mpuInterrupt = false;
 
 /* Objects declaration*/
 // OLED 128x64
@@ -27,14 +40,18 @@ U8G2_SSD1306_128X64_NONAME_F_HW_I2C display(U8G2_R0, U8X8_PIN_NONE);
 // TCS34725
 Adafruit_TCS34725softi2c leftColor = Adafruit_TCS34725softi2c(colorVelocity, TCS34725_GAIN_1X, colorPins[0], colorPins[1]);
 Adafruit_TCS34725softi2c rightColor = Adafruit_TCS34725softi2c(colorVelocity, TCS34725_GAIN_1X, colorPins[2], colorPins[3]);
+// MPU6050
+MPU6050 mpu;
 
 /* Function declaration section */
-// IR Sensor Reader receives the mode (0: Digital, 1: Analog) and the sensor pin
-int irSensorRead(int mode, int pin);
+// IR Sensor Reader receives the sensor pin
+int irSensorRead(int pin);
 // Ultrassonic Sensor Reader receives the trigger and the echo pin
 float ultraSensorRead(int trigPin, int echoPin);
 // Color Sensor Reader receives the side to be read (0: Left, 1: Right) and the color to be read (0: Red, 1: Green, 2: Blue, 3: Clarity)
 int colorSensorRead(int side, int color);
+// MPU6050 Sensor Reader receives the axis to return (1: x, 2: y, 3: z)
+float mpuSensorRead(int axis);
 // Stepper Controller receiver if the movement will be in a cross axis, the delay between the steps, the step quantity, the direction (0, 1, 2, 3) if the cross is true and the angle if it isn't
 bool stepperControl(bool cross, int velocity, int steps, int direction, int angle);
 // Servo Controller receives which servo to control and the movement method
@@ -51,27 +68,41 @@ bool pinsSetup();
 bool oledSetup();
 // TCS34725 setup
 bool colorSetup();
+// MPU6050 setup
+bool mpuSetup();
+// MPU6050 Ambient Test
+void dmpDataReady() { mpuInterrupt = true; }
 
 /* Main code section */
 void setup() {
+  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    Wire.begin();
+    Wire.setClock(400000);
+  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+    Fastwire::setup(400, true);
+  #endif
   Serial.begin(115200);
+  while (!Serial);
   tryFunction(pinsSetup(), "pinsSetup()", "Pins setup Error");
   if (oledDisplay) {
     tryFunction(oledSetup(), "oledSetup()", "OLED 128x64 Malfunction");
   }
   //tryFunction(colorSetup(), "colorSetup()", "TCS34725 Malfunction");
+  //tryFunction(mpuSetup(), "mpuSetup()", "MPU6050 Malfunction");
   infoPrint();
 }
 void loop() {
+  for (int i = 0; i < sizeof(irSensorPins); i++){
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.println(irSensorRead(irSensorPins[i]));
+  }
+  delay(50);
 }
 
 /* Functions section */
-int irSensorRead(int mode, int pin) {
-  if (mode == 0) {
-    return digitalRead(pin);
-  } else if (mode == 1) {
-    return analogRead(pin);
-  }
+int irSensorRead(int pin) {
+  return digitalRead(pin);
 }
 float ultraSensorRead(int trigPin, int echoPin) {
   digitalWrite(trigPin, LOW);
@@ -79,7 +110,7 @@ float ultraSensorRead(int trigPin, int echoPin) {
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  return pulseIn(echoPin, HIGH) * 0.034 / 2;
+  return pulseIn(echoPin, HIGH) * 0.0343 / 2;
 }
 int colorSensorRead(int side, int color) {
   uint16_t r, g, b, c;
@@ -118,6 +149,24 @@ int colorSensorRead(int side, int color) {
       break;
     }
     break;
+  }
+}
+float mpuSensorRead(int axis) {
+  if (!dmpReady) return;
+  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) {
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    switch (axis)
+    {
+    case 1:
+      return q.x;
+      break;
+    case 2:
+      return q.y;
+      break;
+    case 3:
+      return q.z;
+      break;
+    }
   }
 }
 bool stepperControl(bool cross, int velocity, int steps, int direction, int angle) {
@@ -257,4 +306,29 @@ bool colorSetup() {
   } else {
     return false;
   }
+}
+bool mpuSetup() {
+  pinMode(INTERRUPT_PIN, INPUT);
+  mpu.testConnection();devStatus = mpu.dmpInitialize();
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1788); 
+  if (devStatus == 0) {
+    mpu.CalibrateAccel(6);
+    mpu.CalibrateGyro(6);
+    mpu.PrintActiveOffsets();
+    mpu.setDMPEnabled(true);
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+    dmpReady = true;
+    packetSize = mpu.dmpGetFIFOPacketSize();
+    return true;
+  } else {
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+    return false;
+  }
+  return false;
 }
